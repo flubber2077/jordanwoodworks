6453b8741e69eda666523adbf68a9c68207578cd
Dylan <kyle.dylan.jordan@gmail.com>
2022-12-06 00:15:54 -0500

    Footer Reformat

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <Container class="text-bg-primary" fluid="sm" id="contact">
             <div>
                 <h1>Contact</h1>
-                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fermentum turpis ac iaculis. Pellentesque bibendum orci sed fermentum bibendum. Maecenas cursus consequat arcu sagittis egestas. Suspendisse id lorem leo. Nulla pulvinar ipsum eu risus aliquam condimentum. Proin sed orci eu nisl posuere placerat. Suspendisse luctus, ligula nec ornare fringilla, dui massa commodo magna, non varius mi turpis vel justo. Pellentesque dictum quam scelerisque urna commodo, luctus varius tortor ornare. Nullam viverra ut metus in iaculis. Maecenas vitae libero at turpis porttitor dignissim sit amet et turpis. Duis eleifend neque ac lectus consectetur pharetra. Mauris non dolor vel urna dignissim sagittis. Sed vulputate sapien eget varius lobortis.</h4>
+                <h4>Questions, Comments, Commissions, or General Inquiries?</h4>
             </div>
         </Container>
     )
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,58 +10,51 @@ const Footer = () => {
     const svgFormat = { width: '3rem', height: '3rem' }
 
     return (
-        <footer className="page-footer font-small blue pt-4">
-            <Container>
-                <div className="container-fluid text-center text-md-left">
-                    <div className="row">
-                        <div className="col-md-6 mt-md-0 mt-3">
-                            <h5 className="text-uppercase">Footer Content</h5>
-                            <p>Talk to ken about whether to obscure phone number and email or not</p>
-                        </div>
+        <Container fluid='sm'>
+            <Row>
+                <Col>
+                    <h5 className="text-uppercase">Footer Content</h5>
+                    <p>I am happy to talk about </p>
+                </Col>
 
-                        <hr className="clearfix w-100 d-md-none pb-0" />
+                <Col>
+                    <h5 className="text-uppercase">Get In Touch</h5>
+                    <ul className="list-unstyled text-start">
+                        <li>
+                            <Container fluid="true">
+                                <Row>
+                                    <Col><a href="https://www.facebook.com/JordanwoodworkNC/"><img src={fb} style={svgFormat} alt="icon of Facebook logo" /></a></Col>
+                                    <Col><a href="tel:1-919-606-3782"><img fluid="true" src={phone} style={svgFormat} alt="icon of telephone" /></a></Col>
+                                    <Col><a href="mailto:YOUREMAIL@EMAILADDRESS.com"><img fluid="true" src={email} style={svgFormat} alt="icon of mail for email" /></a></Col>
+                                </Row>
+                            </Container>
+                        </li>
+                        <li >
+                            <a href="https://www.facebook.com/JordanwoodworkNC/"><p>facebook.com/JordanwoodworkNC/</p></a>
+                        </li>
+                        <li>
+                            <a href="tel:1-919-606-3782"><p>919-606-3782</p></a>
+                        </li>
+                        <li>
+                            <a href="mailto:YOUREMAIL@EMAILADDRESS.com"><p>ken@jordanwoodworks.com</p></a>
+                        </li>
 
-                        <div className="col-md-3 mb-md-0 mb-3">
-                            <h5 className="text-uppercase">Sitemap</h5>
-                            <ul className="list-unstyled">
-                                <li><a href="#gallery">Gallery</a></li>
-                                <li><a href="#about">About</a></li>
-                                <li><a href="#contact">Contact</a></li>
-                            </ul>
-                        </div>
+                    </ul>
+                </Col>
+                <Col>
+                    <h5 className="text-uppercase">Sitemap</h5>
+                    <ul className="list-unstyled">
+                        <li><a href="#gallery">Gallery</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </Col>
+            </Row>
 
-                        <div className="col-md-3 mb-md-0 mb-3">
-                            <h5 className="text-uppercase">Get In Touch</h5>
-                            <ul className="list-unstyled">
-                                <li>
-                                    <Container fluid="true">
-                                        <Row>
-                                            <Col><a href="https://www.facebook.com/JordanwoodworkNC/"><img src={fb} style={svgFormat} alt="icon of Facebook logo"/></a></Col>
-                                            <Col><a href="tel:1-919-606-3782"><img fluid="true" src={phone} style={svgFormat} alt="icon of telephone"/></a></Col>
-                                            <Col><a href="mailto:YOUREMAIL@EMAILADDRESS.com"><img fluid="true" src={email} style={svgFormat} alt="icon of mail for email"/></a></Col>
-                                        </Row>
-                                    </Container>
-                                </li>
-                                <li >
-                                    <a href="https://www.facebook.com/JordanwoodworkNC/"><p>facebook.com/JordanwoodworkNC/</p></a>
-                                </li>
-                                <li>
-                                    <a href="tel:1-919-606-3782"><p>919-606-3782</p></a>
-                                </li>
-                                <li>
-                                    <a href="mailto:YOUREMAIL@EMAILADDRESS.com"><p>ken@jordanwoodworks.com</p></a>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="footer-copyright text-center py-3">
-                    © 2022 Copyright: Jordan Woodworks
-                </div>
-            </Container>
-        </footer>
+            <div className="footer-copyright text-center py-3">
+                © 2022 Copyright: Jordan Woodworks
+            </div>
+        </Container>
     )
 }
 
