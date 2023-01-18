5c6179690487b9e3faea84ee7bfde49ab040dc4f
Dylan <kyle.dylan.jordan@gmail.com>
2023-01-18 13:29:25 -0500

    edit of images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 // import './App.css';
 import Gallery from './Components/Gallery';
-import Container from 'react-bootstrap/Container';
-import Contact from './Components/Contact';
 import header from './Resources/banner.jpg';
-import headerSmall from './Resources/headerSmall.jpeg';
 import About from './Components/About';
 import Image from 'react-bootstrap/Image';
 import Navbar from './Components/Navbar.js';
